6d1b482c88a3ee938e6c9a5ea55f89ad9bb061db
Alansillo <Alansillo@gmail.com>
2026-03-15 00:02:27 -0600

    Fix puerto para Railway

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-
 import { app } from './app.js'
 import { initBaseDeDatos } from './bd/init.js'
-
 
 /**
  * Iniciar el servidor Express después de conectar a la base de datos
  */
 try {
   await initBaseDeDatos()
-  const PORT = process.env.PORT //|| 3001
-  app.listen(PORT)
-  console.info(`Servidor Express ejecutandose sobre http://localhost:${PORT}`)
+
+  const PORT = process.env.PORT || 3001
+
+  app.listen(PORT, () => {
+    console.info(`Servidor Express ejecutándose sobre http://localhost:${PORT}`)
+  })
 } catch (err) {
-  console.error('Error conecteando a la Base de Datos:', err)
+  console.error('Error conectando a la Base de Datos:', err)
 }
